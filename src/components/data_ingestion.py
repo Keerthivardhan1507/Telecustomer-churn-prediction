1925d43d363df023bf633f26af10a14bf42a2163
Keerthivardhan1507 <nettemKeerthivardhannaidu1507@gmail.com>
2025-09-16 17:13:09 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -52,3 +52,6 @@ class DataIngestion:
 if __name__ == "__main__":
     obj=DataIngestion()
     train_data,test_data =obj.initiate_data_ingestion()
+    
+    data_transfromation = DataTransformation()
+    train_arr,test_arr,_=data_transfromation.initiate_data_transformation(train_data,test_data)
