de009f4ee69650ad04daa9b67e3ae4fec811c4b9
Keerthivardhan1507 <nettemKeerthivardhannaidu1507@gmail.com>
2025-09-17 12:54:17 +0530

    Model done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -5,7 +5,9 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder, MinMaxScaler
+from sklearn.feature_selection import SelectKBest, chi2
+
 
 from src.exception import CustomException
 from src.logger import logging
@@ -48,7 +50,7 @@ class DataTransformation:
             num_pipeline = Pipeline(
                 steps=[
                     ("imputer", SimpleImputer(strategy="median")),
-                    ("scaler", StandardScaler())
+                    ("scaler", MinMaxScaler()) # Using MinMaxScaler to ensure non-negative values for chi2
                 ]
             )
 
@@ -69,8 +71,13 @@ class DataTransformation:
                     ("cat_pipeline", cat_pipeline, categorical_columns)
                 ]
             )
+            
+            # Create a new pipeline with feature selection
+            feature_selection_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                                        ('feature_selection', SelectKBest(score_func=chi2, k=10))])
 
-            return preprocessor
+
+            return feature_selection_pipeline
 
         except Exception as e:
             raise CustomException(e, sys)
@@ -111,7 +118,7 @@ class DataTransformation:
                 "Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df, target_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
